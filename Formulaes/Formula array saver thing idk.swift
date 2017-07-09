f6b59915b1e646c68a7420ba6656dfc717ae5723
SuperGodMichael <MichaelKenMiyajima@gmail.com>
2017-07-08 19:03:31 -0700

    finished original thing which is awesome. now moving to storyboard shenanigans

--- Formulaes/Formula array saver thing idk.swift
+++ Formulaes/Formula array saver thing idk.swift
@@ -7,15 +7,8 @@
 //
 
 import Foundation
-//Formula Struct
-struct Formula{
-    var inputs:[String]
-    var formula:String
-}
-//Array of Formulas
-
 //String into usable formula
-func StringtoFormula(Formula:String){
+func StringtoFormula(Formula:String) ->Formulae{
     //want to put string into formula form, with ,s between formula and inputs
     var charsinformula = Formula
     var inputs1:[String]=[]
@@ -28,26 +21,138 @@ func StringtoFormula(Formula:String){
     var digitCount = 0
     var greekCount = 0
     //finalString has variables replaced with japanese letters with numbers for lolsははははは
-    
+    var beforeequal = true
     var finalString = Array(charsinformula.characters)
     var finalString1 = Array(charsinformula.characters)
     var indexforabovestring = 0
     for uni in charsinformula.unicodeScalars {
-        if letters.contains(uni) {
-            letterCount += 1
-            var strofcharatindex = String(finalString[indexforabovestring])
-            inputs1.append(strofcharatindex)
-            formulastring+="あ\(indexforabovestring)"
-        } else if digits.contains(uni) {
-            digitCount += 1
+        if(beforeequal == true){
+            if letters.contains(uni) {
+                letterCount += 1
+                var strofcharatindex = String(finalString[indexforabovestring])
+                inputs1.append(strofcharatindex)
+                formulastring+="い"
+            } else if digits.contains(uni) {
+                digitCount += 1
+                formulastring+=String(finalString[indexforabovestring])
+            }else if greekletters.contains(uni) {
+                greekCount += 1
+                formulastring+=String(finalString[indexforabovestring])
+            }
+            else if sqrt.contains(uni){
+                formulastring+=String(finalString[indexforabovestring])
+            }
+            else if String(finalString[indexforabovestring]) == "="{
+                beforeequal = false
+                formulastring+="="
+            }
+            else {
+                formulastring+=String(finalString[indexforabovestring])
+            }
             
-        }else if greekletters.contains(uni) {
-            greekCount += 1
         }
-        else if sqrt.contains(uni){
+        else{
+            formulastring+="お"
+        }
+        indexforabovestring+=1
+        
+    }
+    print(Formula+" is "+formulastring)
+    let formula = Formulae(inputs:inputs1, formula:formulastring)
+    return formula
+}
+//Formula Struct
+struct Formulae{
+    var inputs:[String]
+    var formula:String
+    func run(_ inputs:[Int]){
+        var result:Int = 0
+        let digits = CharacterSet.decimalDigits
+        var finalString = Array(formula.characters)
+        var index = 0
+        var indexofinputs = 0
+        var temp = 0
+        var signisadd = false
+        var signissubtract = false
+        var signismultiply = false
+        var signisdivide = false
+        for character in finalString{
+            print(result)
             
+            if String(character) == "+"{
+                signisadd = true
+                print("sign is add")
         }
-     indexforabovestring+=1
+            if String(character) == "-"{
+                signissubtract = true
+            print("sign is sub")
+    }
+            if String(character) == "*"{
+                signismultiply = true
+                print("sign is *")
+            }
+            if String(character) == "/"{
+                signisdivide = true
+                print("sign is /")
+            }
+               
+        
+            if String(character) == "い"{
+                print("at input")
+                temp = inputs[indexofinputs]
+                if result == 0{
+                    result = inputs[indexofinputs]
+                    print("result was 0 so i changed it to myself")
+                }
+                
+                
+                
+                    if signisadd == true{
+                    result += temp
+                        print("added to results")
+                    }
+                    if signismultiply == true{
+                        result = result*temp
+                        print("multiplied result to temp")
+                    }
+                    if signissubtract == true{
+                        result = result - temp
+                        print("subbed temp from result")
+                    }
+                    if signisdivide == true{
+                        result = result/temp
+                        print("divided result by temp")
+                    }
+                
+                
+            indexofinputs+=1
+            }
+            if digits.contains(UnicodeScalar(String(character))!){
+                if result == 0{
+                    result = Int(String(character))!
+                }
+                temp = Int(String(character))!
+                if signisadd == true{
+                    result += temp
+                }
+                if signismultiply == true{
+                    result = result*temp
+                }
+                if signissubtract == true{
+                    result = result - temp
+                }
+                if signisdivide == true{
+                    result = result/temp
+                }
+                
+            }
+            index += 1
+        }
+        print(result)
     }
     
 }
+//Array of Formulas
+
+
+    
--- Formulaes/ViewController.swift
+++ Formulaes/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var Formulaarray:[Formulae] = []
 var egformula = "x*3=t"
-var quadformula = "√x=x"
+var quadformula = "(-b+-√b^2-4ac)/2a=t"
+    var testmultipleinputs = "t+a+b=c"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        StringtoFormula(Formula:egformula)
+        Formulaarray.append(StringtoFormula(Formula:egformula))
+        Formulaarray.append(StringtoFormula(Formula:quadformula))
+        Formulaarray.append(StringtoFormula(Formula:testmultipleinputs))
+        var testformula = StringtoFormula(Formula:testmultipleinputs)
+        print(Formulaarray)
+        var testintarray = [4,1,2]
+        testformula.run(testintarray)
     }
 
     override func didReceiveMemoryWarning() {
